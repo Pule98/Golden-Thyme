a29347964da575eb84d4571d2d7e3cbc96427faf
Pule-Taunyane <Pule.taunyane98@gmail.com>
2023-02-08 14:13:00 +0200

    New folders and files

---
+++ Golden Thyme/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Golden_Thyme
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int clickCounter = 0;
+        private const int MAX_Clicks = 3;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clickCounter++;
+            if (clickCounter >= MAX_Clicks)
+            {
+                button1.Enabled = false;
+            }
+            int userNum = Convert.ToInt32(userNumberTxb.Text);
+            int systemNumber = rand.Next(1,31);
+
+            if (userNum == systemNumber)
+            {
+                resultslbl.Text = "WON";
+            }
+            else
+            {
+                resultslbl.Text = "LOST";
+            }
+
+            jackpotNumber.Text = systemNumber.ToString();
+
+            
+           
+        }
+    }
+}
