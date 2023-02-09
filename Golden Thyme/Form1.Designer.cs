081594e4eeb87a5a6d7eb67486688e1e45acd102
Pule-Taunyane <Pule.taunyane98@gmail.com>
2023-02-09 13:26:09 +0200

    Edited code

--- Golden Thyme/Form1.Designer.cs
+++ Golden Thyme/Form1.Designer.cs
@@ -36,6 +36,11 @@ namespace Golden_Thyme
             this.button1 = new System.Windows.Forms.Button();
             this.jackpotNumber = new System.Windows.Forms.Label();
             this.resultslbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cashamounttxt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.cashamountlbl = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,18 +56,18 @@ namespace Golden_Thyme
             // userNumberTxb
             // 
             this.userNumberTxb.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userNumberTxb.Location = new System.Drawing.Point(251, 239);
+            this.userNumberTxb.Location = new System.Drawing.Point(247, 307);
             this.userNumberTxb.MaxLength = 2;
             this.userNumberTxb.Name = "userNumberTxb";
             this.userNumberTxb.Size = new System.Drawing.Size(123, 29);
-            this.userNumberTxb.TabIndex = 1;
+            this.userNumberTxb.TabIndex = 2;
             this.userNumberTxb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(97, 242);
+            this.label1.Location = new System.Drawing.Point(34, 307);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(148, 23);
             this.label1.TabIndex = 2;
@@ -73,11 +78,11 @@ namespace Golden_Thyme
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.button1.Location = new System.Drawing.Point(12, 284);
+            this.button1.Location = new System.Drawing.Point(12, 370);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(419, 29);
+            this.button1.Size = new System.Drawing.Size(92, 29);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Bet";
+            this.button1.Text = "Bet 5";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -85,26 +90,89 @@ namespace Golden_Thyme
             // 
             this.jackpotNumber.AutoSize = true;
             this.jackpotNumber.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.jackpotNumber.Location = new System.Drawing.Point(200, 334);
+            this.jackpotNumber.Location = new System.Drawing.Point(194, 444);
             this.jackpotNumber.Name = "jackpotNumber";
-            this.jackpotNumber.Size = new System.Drawing.Size(0, 23);
+            this.jackpotNumber.Size = new System.Drawing.Size(34, 23);
             this.jackpotNumber.TabIndex = 4;
+            this.jackpotNumber.Text = "dfdf";
             // 
             // resultslbl
             // 
             this.resultslbl.AutoSize = true;
             this.resultslbl.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resultslbl.Location = new System.Drawing.Point(200, 381);
+            this.resultslbl.Location = new System.Drawing.Point(194, 491);
             this.resultslbl.Name = "resultslbl";
-            this.resultslbl.Size = new System.Drawing.Size(0, 23);
+            this.resultslbl.Size = new System.Drawing.Size(34, 23);
             this.resultslbl.TabIndex = 5;
+            this.resultslbl.Text = "dfdf";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter your amount";
+            // 
+            // cashamounttxt
+            // 
+            this.cashamounttxt.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cashamounttxt.Location = new System.Drawing.Point(247, 245);
+            this.cashamounttxt.MaxLength = 9000000;
+            this.cashamounttxt.Name = "cashamounttxt";
+            this.cashamounttxt.Size = new System.Drawing.Size(123, 29);
+            this.cashamounttxt.TabIndex = 1;
+            this.cashamounttxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button2.Location = new System.Drawing.Point(153, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Bet 10";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button3.Location = new System.Drawing.Point(310, 370);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Bet 15";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // cashamountlbl
+            // 
+            this.cashamountlbl.AutoSize = true;
+            this.cashamountlbl.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cashamountlbl.Location = new System.Drawing.Point(25, 415);
+            this.cashamountlbl.Name = "cashamountlbl";
+            this.cashamountlbl.Size = new System.Drawing.Size(34, 23);
+            this.cashamountlbl.TabIndex = 10;
+            this.cashamountlbl.Text = "dfdf";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(443, 414);
+            this.ClientSize = new System.Drawing.Size(443, 545);
+            this.Controls.Add(this.cashamountlbl);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cashamounttxt);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.resultslbl);
             this.Controls.Add(this.jackpotNumber);
             this.Controls.Add(this.button1);
@@ -131,6 +199,11 @@ namespace Golden_Thyme
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label jackpotNumber;
         private System.Windows.Forms.Label resultslbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox cashamounttxt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label cashamountlbl;
     }
 }
 
--- Golden Thyme/Form1.cs
+++ Golden Thyme/Form1.cs
@@ -27,8 +27,50 @@ namespace Golden_Thyme
             {
                 button1.Enabled = false;
             }
+
+            string text = cashamounttxt.Text;
+            int number = int.Parse(text);
+            number = number - 5;
+            
+
             int userNum = Convert.ToInt32(userNumberTxb.Text);
             int systemNumber = rand.Next(1,31);
+            if (cashamounttxt.Text.Length <= 2)
+            {
+
+            if (userNum == systemNumber)
+            {
+                resultslbl.Text = "WON";
+            }
+            else
+            {
+                resultslbl.Text = "LOST";
+            }
+            }
+            else
+            {
+                MessageBox.Show("Minimum amount R10.00");
+            }
+            if(number == 0)
+            {
+                MessageBox.Show("Insert more money");
+            }
+            cashamountlbl.Text = number.ToString();
+            jackpotNumber.Text = systemNumber.ToString();
+
+            
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clickCounter++;
+            if (clickCounter >= MAX_Clicks)
+            {
+                button1.Enabled = false;
+            }
+            int userNum = Convert.ToInt32(userNumberTxb.Text);
+            int systemNumber = rand.Next(1, 31);
 
             if (userNum == systemNumber)
             {
@@ -41,8 +83,31 @@ namespace Golden_Thyme
 
             jackpotNumber.Text = systemNumber.ToString();
 
-            
-           
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            clickCounter++;
+            if (clickCounter >= MAX_Clicks)
+            {
+                button1.Enabled = false;
+            }
+            int userNum = Convert.ToInt32(userNumberTxb.Text);
+            int systemNumber = rand.Next(1, 31);
+
+            if (userNum == systemNumber)
+            {
+                resultslbl.Text = "WON";
+            }
+            else
+            {
+                resultslbl.Text = "LOST";
+            }
+
+            jackpotNumber.Text = systemNumber.ToString();
+
+        }
+
+
     }
 }
